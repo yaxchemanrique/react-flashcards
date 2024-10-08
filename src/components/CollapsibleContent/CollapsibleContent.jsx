8778b92e6283db4abd2d776611baa3edb7dab245
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-10-08 10:01:15 -0700

    fix: collapsible button

--- src/components/CollapsibleContent/CollapsibleContent.jsx
+++ src/components/CollapsibleContent/CollapsibleContent.jsx
@@ -1,24 +1,27 @@
 import { useState } from "react";
 import { ChevronDown, ChevronUp } from "react-feather";
-import styles from './CollapsibleContent.module.css'
+import styles from "./CollapsibleContent.module.css";
 
 function CollapsibleContent({ title, tag, children }) {
   const [isCollapsed, setIsCollapsed] = useState(true);
   return (
     <div>
-      <div className={styles.titleContainer} style={{ display: "flex", justifyContent: "space-between" }}>
+      <button
+        onClick={() => setIsCollapsed((current) => !current)}
+        className={styles.collapseButton}
+      >
         <div className={styles.titleTextContainer}>
           <span className={styles.title}>{title}</span>
           <span className={styles.tag}>{tag}</span>
         </div>
-        <button className={styles.button} onClick={() => setIsCollapsed((current) => !current)}>
+        <div className={styles.iconContainer}>
           {isCollapsed ? <ChevronDown /> : <ChevronUp />}
-        </button>
-      </div>
-      <div 
-      className={styles.collapsedContent}
+        </div>
+      </button>
+      <div
+        className={styles.collapsedContent}
         style={{
-          maxHeight: isCollapsed ? 0 : "100vh"
+          maxHeight: isCollapsed ? 0 : "100vh",
         }}
       >
         {children}
